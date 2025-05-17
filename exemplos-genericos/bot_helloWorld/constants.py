a62791ed1184900fb049b41e12601614c50e969a
Thayna Costa <thaynab170@gmail.com>
2025-05-17 16:24:10 -0400

    feat: adicione o módulo onde fica armazenado as constantes e seus respectivos valores

---
+++ exemplos-genericos/bot_helloWorld/constants.py
@@ -0,0 +1,7 @@
+APP_PATH =  r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk'
+
+WAITING_TIME = 2000
+INTERVAL_KEYS = 2
+
+TEXT = "Hello World"
+FILE_NAME = "C:\\Users\\thayn\\Documents\\projetos_github\\guia-academy\\exemplos-genericos\\bot_helloWorld\\hello-world.txt"
