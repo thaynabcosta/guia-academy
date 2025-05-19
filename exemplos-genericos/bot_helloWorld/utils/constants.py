d2322945c2670683f112733d2afbffeb170edd5d
Thayna Costa <thaynab170@gmail.com>
2025-05-19 14:58:15 -0400

    feat: deixar o nome do arquivo mais dinâmico, buscando o diretório de onde o projeto está salvo

--- exemplos-genericos/bot_helloWorld/utils/constants.py
+++ exemplos-genericos/bot_helloWorld/utils/constants.py
@@ -1,7 +1,9 @@
+import os
+
 APP_PATH =  r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk'
 
 WAITING_TIME = 2000
 INTERVAL_KEYS = 2
 
 TEXT = "Hello World"
-FILE_NAME = "C:\\Users\\thayn\\Documents\\projetos_github\\guia-academy\\exemplos-genericos\\bot_helloWorld\\hello-world.txt"
+FILE_NAME = os.path.join(os.getcwd(), "hello-world.txt")
